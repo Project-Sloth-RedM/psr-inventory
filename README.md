72540d003c9f361a461d351c80d86bd163a1a9de
RexShack <rexshackuk@gmail.com>
2022-09-26 23:47:53 +0100

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# psr-inventory
---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'PSR-Inventory'
+
+shared_scripts {
+    '@psr-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
+    '@qbr-weapons/config.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+client_script 'client/main.lua'
+
+ui_page {
+    'html/ui.html'
+}
+
+files {
+    'html/ui.html',
+    'html/css/main.css',
+    'html/js/app.js',
+    'html/images/*.png',
+    'html/images/*.jpg',
+    'html/ammo_images/*.png',
+    'html/attachment_images/*.png',
+    'html/*.ttf'
+}
+
+lua54 'yes'
